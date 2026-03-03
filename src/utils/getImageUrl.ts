6497a631dbe3548d9e39772e5238ab4d49fc555b
Cassio Souza <cassio.souza@ancar.local>
2026-03-03 12:19:49 -0300

    fix: debugando a imagem

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,12 +1,9 @@
-// src/utils/getImageUrl.ts
 export function getImageUrl(path?: string) {
   if (!path) return '/placeholder.png';
 
-  // Se já for URL completa (ex: http://...)
   if (path.startsWith('http')) return path;
 
-  const baseUrl = import.meta.env.VITE_API_URL;
+  const baseUrl = import.meta.env.VITE_API_URL?.replace(/\/$/, '');
 
-  // Garante que sempre tenha /uploads/
-  return `${baseUrl}/uploads/${path}`;
+  return `${baseUrl}${path}`;
 }
