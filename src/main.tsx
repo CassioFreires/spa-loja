257e1ae439e2d8145343c0317207d172473e675c
Cassio Souza <cassio.souza@ancar.local>
2026-03-05 15:53:24 -0300

    fix: build: estabilização do deploy e correção de rotas na raiz

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './context/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/spa-loja'>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
